0eb89c20021e9d0d9b8c8f49e6a550bb3a93d8f7
Milo <miloslav.smicik@gmail.com>
2020-04-11 13:28:24 +0200

    function run, passing config by argument

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
-use sdl2::rect::Rect;
 
-const TITLE: &str = "dinai";
-const WIDTH: u32 = 1280;
-const HEIGHT: u32 = 720;
+struct Config {
+    title: &'static str,
+    width: u32,
+    height: u32,
+}
 
 fn render(canvas: &mut Canvas<Window>) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(240, 240, 240));
     canvas.clear();
-
-    canvas.set_draw_color(Color::RGB(0, 128, 0));
-    canvas.fill_rect(Rect::new(0, (HEIGHT as i32) * 4 / 5, WIDTH, 5))?;
 
     canvas.present();
 
@@ -22,15 +20,27 @@ fn render(canvas: &mut Canvas<Window>) -> Result<(), String> {
 }
 
 fn main() -> Result<(), String> {
+    let conf = Config {
+        title: "default title",
+        width: 450,
+        height: 450,
+    };
+
+    run(conf)
+}
+
+fn run(config: Config) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window(TITLE, WIDTH, HEIGHT)
+    let window = video_subsystem
+        .window(config.title, config.width, config.height)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas()
+    let mut canvas = window
+        .into_canvas()
         .present_vsync()
         .build()
         .map_err(|e| e.to_string())?;
@@ -40,10 +50,11 @@ fn main() -> Result<(), String> {
     'main_loop: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Q), .. } => {
-                    break 'main_loop
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Q),
+                    ..
+                } => break 'main_loop,
                 _ => {}
             }
         }
@@ -53,4 +64,3 @@ fn main() -> Result<(), String> {
 
     Ok(())
 }
-
